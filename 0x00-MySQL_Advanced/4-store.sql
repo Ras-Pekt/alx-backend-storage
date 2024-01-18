5308ebeb88322dc848b6c1968aa94671fe30766d
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-01-18 14:37:17 +0300

    fix:create a trigger that decreases the quantity of an item after adding a new order

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,7 +5,8 @@ AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
 	UPDATE items
-	SET quantity = quantity - NEW.order_quantity
+	SET quantity = quantity - NEW.number
 	WHERE name = NEW.item_name
-END;$$
+END;
+$$
 DELIMITER ;
