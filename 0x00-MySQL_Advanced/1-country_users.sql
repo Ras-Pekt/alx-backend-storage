f66d6bb5db07dd6a048c72fc84d65a037d4bb4e6
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-01-18 13:01:52 +0300

    create a table users with enum attribute

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- a SQL script that creates a table users with enum attribute 
+CREATE TABLE IF NOT EXISTS users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM("US", "CO", "TN") NOT NULL DEFAULT "US"
+);
--- 0x00-MySQL_Advanced/README.md
+++ 0x00-MySQL_Advanced/README.md
@@ -1,2 +1,3 @@
 # 0x00. MySQL advanced
 - [0-uniq_users.sql](0-uniq_users.sql) - creates a table users on any database
+- [1-country_users.sql](1-country_users.sql) - creates a table users with enum attribute on any database
