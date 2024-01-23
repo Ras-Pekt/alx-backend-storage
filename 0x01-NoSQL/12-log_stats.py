b6c0c9472c40dd20cd1847296448b8ed3cdc04d6
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-01-23 19:56:00 +0300

    fix:provide some stats about Nginx logs stored in MongoDB

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,18 +2,20 @@
 """
 a Python script that provides some stats about Nginx logs stored in MongoDB
 """
-from pymongo import MongoClient
 
-client = MongoClient("mongodb://127.0.0.1:27017")
-db_logs = client.logs
-db_collections = db_logs.nginx
+if __name__ == "__main__":
+    from pymongo import MongoClient
 
-print(f"{db_collections.count_documents({})} logs\nMethods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    logs = client.logs
+    collections = logs.nginx
 
-for method in methods:
-    count = db_collections.count_documents({'method': method})
-    print(f"\tmethod {method}: {count}")
+    print(f"{collections.count_documents({})} logs\nMethods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-count = db_collections.count_documents({'method': 'GET', 'path': '/status'})
-print(f"{count} status check")
+    for method in methods:
+        count = collections.count_documents({'method': method})
+        print(f"\tmethod {method}: {count}")
+
+    count = collections.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{count} status check")
