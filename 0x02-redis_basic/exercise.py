e31d39aa6c801950188f613d9f0db75a99efa04d
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2024-01-24 17:49:51 +0300

    read from Redis and recover original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ a Cache class module
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -26,3 +26,23 @@ class Cache:
         uniq_id = str(uuid.uuid4())
         self._redis.set(uniq_id, data)
         return uniq_id
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> str:
+        """
+        takes a key string argument and an optional Callable argument
+        """
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """
+        automatically parametrize Cache.get
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        automatically parametrize Cache.get
+        """
+        return self.get(key, fn=int)
